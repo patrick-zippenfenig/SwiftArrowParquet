f2725c4bc13d6a5f40768a4852769308ec44dbc5
patrick-zippenfenig <patrick@zippenfenig.de>
2025-06-03 17:43:16 +0200

    fix: crash in release build due to early release of [ArrowArray]

--- Sources/SwiftArrowParquet/ArrowTable.swift
+++ Sources/SwiftArrowParquet/ArrowTable.swift
@@ -5,14 +5,15 @@ public final class ArrowTable {
     let ptr: UnsafeMutablePointer<GArrowTable>
     
     public init(schema: ArrowSchema, arrays: [ArrowArray]) throws {
-        var error: UnsafeMutablePointer<GError>? = nil
-        var arrayPtr: [UnsafeMutablePointer<GArrowArray>?] = arrays.map { $0.ptr }
-        
-        guard let table = garrow_table_new_arrays(schema.ptr, &arrayPtr, UInt(arrays.count), &error) else {
-            defer { g_error_free(error)}
-            throw ArrowError.tableError(message: error.map {String(cString: $0.pointee.message) } ?? "")
+        ptr = try arrays.withUnsafeBufferPointer { arrays in
+            var error: UnsafeMutablePointer<GError>? = nil
+            var arrayPtr: [UnsafeMutablePointer<GArrowArray>?] = arrays.map { $0.ptr }
+            guard let table = garrow_table_new_arrays(schema.ptr, &arrayPtr, UInt(arrays.count), &error) else {
+                defer { g_error_free(error)}
+                throw ArrowError.tableError(message: error.map {String(cString: $0.pointee.message) } ?? "")
+            }
+            return table
         }
-        ptr = table
     }
     
     /// The number of rows in the table.
